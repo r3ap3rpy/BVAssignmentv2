70f27021d30eb23849ad3bef1b649bb098cb4d62
Szabo <dszabo@itron.com>
2020-02-19 06:35:21 +0100

    Initialized repo

---
+++ README.md
@@ -0,0 +1,1 @@
+"# BVAssignmentv2" 
---
+++ src/test/java/com/example/BVictor/BVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BVictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
